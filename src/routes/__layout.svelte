bc9da3a4128a7342c7dd3cfdc1878cd8cb261edd
cympfh <cympfh@gmail.com>
2021-07-04 16:27:11 +0900

    add Bulma

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,23 @@
-<nav>
-	<a href="./">Top</a>
-	<a href="./about">About</a>
-</nav>
+<script lang="ts">
+	import { page } from '$app/stores';
+	$: path = $page.path;
+</script>
+
+<div class="section">
+	<div class="container">
+		<nav class="breadcrumb" aria-label="breadcrumbs">
+			<ul>
+				<li class:is-active={path == '/'}><a href="./">Top</a></li>
+				<li class:is-active={path == '/about'}><a href="./about">About</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
 
 <slot />
+
+<footer class="footer">footer</footer>
+
+<style global lang="scss">
+	@import '../main.scss';
+</style>
