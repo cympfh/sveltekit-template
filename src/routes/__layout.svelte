84fd5d034f25225382d086332fdd791d8a6c06e1
cympfh <cympfh@gmail.com>
2021-07-14 01:00:07 +0900

    Indent with 2 spaces

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	$: path = $page.path;
+  import { page } from '$app/stores';
+  $: path = $page.path;
 </script>
 
 <div class="section">
-	<div class="container">
-		<nav class="breadcrumb" aria-label="breadcrumbs">
-			<ul>
-				<li class:is-active={path == '/'}><a href="./">Top</a></li>
-				<li class:is-active={path == '/about'}><a href="./about">About</a></li>
-			</ul>
-		</nav>
-	</div>
+  <div class="container">
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+      <ul>
+        <li class:is-active={path == '/'}><a href="./">Top</a></li>
+        <li class:is-active={path == '/about'}><a href="./about">About</a></li>
+      </ul>
+    </nav>
+  </div>
 </div>
 
 <slot />
@@ -19,5 +19,5 @@
 <footer class="footer">I am a footer.</footer>
 
 <style global lang="scss">
-	@import '../main.scss';
+  @import '../main.scss';
 </style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import Icon from 'svelte-awesome';
-	import { question } from 'svelte-awesome/icons';
+  import Icon from 'svelte-awesome';
+  import { question } from 'svelte-awesome/icons';
 </script>
 
 <svelte:head>
-	<title>About - SvelteKit App</title>
+  <title>About - SvelteKit App</title>
 </svelte:head>
 
 <div class="section">
-	<div class="container">
-		<h1 class="title">What This <Icon data={question} scale="2.4" /></h1>
-		<p>This is a sample template for SvelteKit.</p>
-		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-	</div>
+  <div class="container">
+    <h1 class="title">What This <Icon data={question} scale="2.4" /></h1>
+    <p>This is a sample template for SvelteKit.</p>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  </div>
 </div>
